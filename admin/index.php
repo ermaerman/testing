8fea777612b9eaca3a252e7960c07c01eff4f70f
Andrea Damayanti <andr.dmynti22@gmail.com>
2019-04-26 21:47:51 +0700

    index

--- admin/index.php
+++ admin/index.php
@@ -274,17 +274,6 @@
                                         <a href="#">
                                             <div class="hd-message-sn">
                                                 <div class="hd-message-img">
-                                                    <img src="img/post/1.jpg" alt="" />
-                                                </div>
-                                                <div class="hd-mg-ctn">
-                                                    <h3>David Belle</h3>
-                                                    <p>Cum sociis natoque penatibus et magnis dis parturient montes</p>
-                                                </div>
-                                            </div>
-                                        </a>
-                                        <a href="#">
-                                            <div class="hd-message-sn">
-                                                <div class="hd-message-img">
                                                     <img src="img/post/2.jpg" alt="" />
                                                 </div>
                                                 <div class="hd-mg-ctn">
