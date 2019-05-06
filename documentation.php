74b3289ed1498971e6a869629490734a96b49bc0
Erma Noviana <ermafel@gmail.com>
2019-05-06 22:50:34 +0700

    sixteenth commit

--- documentation.php
+++ documentation.php
@@ -1,3 +1,207 @@
+<div class="section">
+  <img src="assets/img/path1.png" class="path">
+          <div class="container">
+            <div class="title">
+              <br>
+            </div>
+            <div class="row justify-content-between align-items-center">
+              <div class="col-lg-5 mb-5 mb-lg-0 ">
+                <h1 class="text-white font-weight-light">Dokumentasi</h1>
+                <p class="text-white mt-4">Sistem analisis ini dibuat dengan antarmuka <i>user friendly</i> yang diharapkan agar dapat selalu memudahkan penggunanya.</p>
+                <button href="#" class="btn btn-icon btn-simple btn-round btn-neutral">
+                </button>
+                <button href="#" class="btn btn-icon btn-simple btn-round btn-neutral">
+                </button>
+                <button href="#" class="btn btn-icon btn-simple btn-round btn-neutral">
+                </button>
+              </div>
+              <div class="col-lg-6">
+                <div id="carouselExampleControls" class="carousel slide">
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                      <img class="d-block w-100" src="assets/img/denys.jpg" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="assets/img/fabien-bazanegue.jpg" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="assets/img/mark-finn.jpg" alt="Third slide">
+                    </div>
+                  </div>
+                  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <i class="tim-icons icon-minimal-left"></i>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <i class="tim-icons icon-minimal-right"></i>
+                    <span class="sr-only">Next</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    
+
+
+
+        
+        <div class="container">
+          <div class="row row-grid justify-content-between">
+            <div class="col-md-5 mt-lg-5">
+              <div class="row">
+                <div class="col-lg-6 col-sm-12 px-2 py-2">
+                  <div class="card card-stats ">
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col-5 col-md-4">
+                          <div class="icon-big text-center icon-warning">
+                            <i class="tim-icons icon-trophy text-warning"></i>
+                          </div>
+                        </div>
+                        <div class="col-7 col-md-8">
+                          <div class="numbers">
+                            <p class="card-title">2019
+                              <p>
+                                <p class="card-category">Analisys System V 1.0.0</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-6 col-sm-12 px-2 py-2">
+                  <div class="card card-stats upper bg-default">
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col-5 col-md-4">
+                          <div class="icon-big text-center icon-warning">
+                            <i class="tim-icons icon-coins text-white"></i>
+                          </div>
+                        </div>
+                        <div class="col-7 col-md-8">
+                          <div class="numbers">
+                            <p class="card-title">3
+                              <p>
+                                <p class="card-category">Level User</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-lg-6 col-sm-12 px-2 py-2">
+                  <div class="card card-stats ">
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col-5 col-md-4">
+                          <div class="icon-big text-center icon-warning">
+                            <i class="tim-icons icon-gift-2 text-info"></i>
+                          </div>
+                        </div>
+                        <div class="col-7 col-md-8">
+                          <div class="numbers">
+                            <p class="card-title">90
+                              <p>
+                                <p class="card-category">Days Making</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-6 col-sm-12 px-2 py-2">
+                  <div class="card card-stats ">
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col-5 col-md-4">
+                          <div class="icon-big text-center icon-warning">
+                            <i class="tim-icons icon-credit-card text-success"></i>
+                          </div>
+                        </div>
+                        <div class="col-7 col-md-8">
+                          <div class="numbers">
+                            <p class="card-title">> 200
+                              <p>
+                                <p class="card-category">Files</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="pl-md-5">
+                <h3 class="text font-weight-light mb-2">Sumber daya dan kredit</h3>
+               <p class="text-white mt-4">Sistem Desain ini sepenuhnya dikodekan dan dibangun di atas Open Source, lebih detail di sini:<br>
+                 <ul>
+          <li>
+            <a href="#">Bootstrap 4</a> - Open source front end framework</li>
+          <li>
+            <a href="#">BLK Design System</a> - Open source CSS Library</li>
+          <li>
+            <a href="#">Notika</a> - Open source CSS Library</li>
+            <li>
+            <a href="#">Creative Tim</a> - Open source design web</li>
+        </ul>
+
+        <p class="text-white mt-4">Ikuti perkembangan perjalanan pengembangan dan terhubung dengan kami di:<br>
+                 <ul>
+          <li>
+            <a href="http://www.damri.co.id">www.damri.co.id</a> - Website Perum DAMRI</li>
+        </ul>
+               </p>
+               </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </section>
+
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-12">
+            <div class="row row-grid justify-content-center">
+              <div class="col-lg-3">
+                <div class="info">
+                  <div class="icon icon-primary">
+                    <i class="tim-icons icon-money-coins"></i>
+                  </div>
+                  <h4 class="info-title">Superadmin</h4>
+                  <hr class="line-primary">
+                  <p>Level ID 1, dapat mengakses semua fitur aplikasi begitu pula dengan manajemen user.</p>
+                </div>
+              </div>
+              <div class="col-lg-3">
+                <div class="info">
+                  <div class="icon icon-warning">
+                    <i class="tim-icons icon-chart-pie-36"></i>
+                  </div>
+                  <h4 class="info-title">Admin Cabang</h4>
+                  <hr class="line-warning">
+                  <p>Level ID 2, diperuntukkan bagi admin cabang yang berkaitan dalam proses input penjualan ticketing segmen antar kota.</p>
+                </div>
+              </div>
+              <div class="col-lg-3">
+                <div class="info">
+                  <div class="icon icon-success">
+                    <i class="tim-icons icon-single-02"></i>
+                  </div>
+                  <h4 class="info-title">General Manager</h4>
+                  <hr class="line-success">
+                  <p>Level ID 3, diperuntukkan bagi General Manager cabang yang berkaitan dalam proses analisis ticketing segmen antar kota.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
 <!-- Form Modal -->
       <div class="modal fade modal-black" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
         <div class="modal-dialog">
